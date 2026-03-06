8739a684cfbcc4b67b805f72d3ef79ee3eeae082
Seema <seemushaikseema@gmail.com>
2026-03-06 10:52:53 +0530

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Navbar from '@/components/layout/Navbar';
 import Footer from '@/components/layout/Footer';
-import Hero from '../components/layout/sections/Hero';
-import Stats from '../components/layout/sections/Stats';
-import Accreditations from '../components/layout/sections/Accreditations';
-import Contact from '../components/layout/sections/Contact';
+import Hero from '@/components/layout/sections/Hero';
+import Stats from '@/components/layout/sections/Stats';
+import Accreditations from '@/components/layout/sections/Accreditations';
+import Contact from '@/components/layout/sections/Contact';
+
 export default function Home() {
   return (
     <>
